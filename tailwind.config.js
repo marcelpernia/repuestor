a0902714c2bf65c4dbc9311c30e37cb2fc4ef8a2
Marcel Pernia <marcelpm1987@gmail.com>
2024-05-31 11:27:14 +0200

    chore: Update ESLint configuration and package dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'brand': {
+          '50': '#fff1f1',
+          '100': '#ffe0e1',
+          '200': '#ffc7c9',
+          '300': '#ffa0a3',
+          '400': '#ff6a6f',
+          '500': '#f83b42',
+          '600': '#e61e25',
+          '700': '#c1141a',
+          '800': '#a01419',
+          '900': '#84181c',
+          '950': '#480709',
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
