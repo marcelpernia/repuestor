52465044f4052b422b21d3b82b8bbd8a596d74d6
Marcel Pernia <marcelpm1987@gmail.com>
2024-06-10 18:35:55 +0200

    se agrega imagen de placeholder a productos sin imagen

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { CldImage } from 'next-cloudinary';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const ProductCard = ( product ) => {
   return (
     <Link href={`/p/${product.slug}`} className="p-2 md:p-4 rounded-md border border-gray-100 space-y-4">
-      {product?.gallery?.data && (
+      {product?.gallery?.data ? (
         <CldImage
           width={200}
           height={200}
@@ -18,6 +19,14 @@ export const ProductCard = ( product ) => {
           background="white"
           border="40px_solid_white"
           format="webp"
+        />
+      ) : (
+        <Image
+          width={200}
+          height={200}
+          src="/placeholder.png"
+          alt={product.title}
+          className="aspect-square object-contain mx-auto"
         />
       )}
       <h2 className='md:text-base text-sm'>{product?.title}</h2>
--- src/app/p/[slug]/page.jsx
+++ src/app/p/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation'
 import { getProductBySlug } from "@/app/services/getProductBySlug";
 import { Img } from "./Img";
+import Image from 'next/image';
 
 export default async function Page({ params }) {
 
@@ -17,10 +18,20 @@ export default async function Page({ params }) {
     <div className="py-10 px-4">
       <div className="container mx-auto max-w-[600px]">
         <div className="space-y-4">
-          <Img 
-            src={p.gallery?.data[0]?.attributes?.url}
-            title={p.title}
-          />
+          {p?.gallery?.data ? (
+            <Img 
+              src={p.gallery?.data[0]?.attributes?.url}
+              title={p.title}
+            />
+          ) : (
+            <Image
+              width={600}
+              height={600}
+              src="/placeholder.png"
+              alt={p.title}
+              className="aspect-square object-contain mx-auto"
+            />
+          )}
           <div className="space-y-1">
             <div className="text-xs md:text-sm">
               <span className="p-1 bg-gray-100 inline-block">SKU: {p.sku}</span>
