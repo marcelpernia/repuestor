97ce89d5efbc47d8b2efd91a13bf32829d870fd1
Marcel Pernia <marcelpm1987@gmail.com>
2024-11-15 13:04:28 +0100

    se agrega persistencia de datos en carrito al recargar pagina

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,39 +1,53 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export const useProductStore = create((set, get) => ({
-  products: [],
-  cart: false,
-  addProduct: ({ id, title, image, price, quantity, stock }) => set((state) => ({
-    products: [
-      ...state.products,
-      {
-        id,
-        title,
-        image,
-        price,
-        quantity,
-        stock
-      }
-    ]
-  })),
-  removeProduct: (id) => set((state) => ({
-    products: state.products.filter(item => item.id !== id)
-  })),
-  updateProductQty: (id, newQty) => set((state) => ({
-    products: state.products.map(product =>
-      product.id === id ? { ...product, quantity: newQty } : product
-    )
-  })),
-  incrementProductQty: (id) => set((state) => ({
-    products: state.products.map(product =>
-      product.id === id && product.quantity < product.stock ? { ...product, quantity: product.quantity + 1 } : product
-    )
-  })),
-  isProductInCart: (id) => {
-    const { products } = get()
-    return products.some(product => product.id === id)
-  },
-  updateCart: (status) => set(() => ({
-    cart: status
-  }))
-}))
+export const useProductStore = create(
+  persist(
+    (set, get) => ({
+      products: [],
+      cart: false,
+
+      addProduct: ({ id, title, image, price, quantity, stock }) =>
+        set((state) => ({
+          products: [
+            ...state.products,
+            { id, title, image, price, quantity, stock }
+          ]
+        })),
+
+      removeProduct: (id) =>
+        set((state) => ({
+          products: state.products.filter((item) => item.id !== id)
+        })),
+
+      updateProductQty: (id, newQty) =>
+        set((state) => ({
+          products: state.products.map((product) =>
+            product.id === id ? { ...product, quantity: newQty } : product
+          )
+        })),
+
+      incrementProductQty: (id) =>
+        set((state) => ({
+          products: state.products.map((product) =>
+            product.id === id && product.quantity < product.stock
+              ? { ...product, quantity: product.quantity + 1 }
+              : product
+          )
+        })),
+
+      isProductInCart: (id) => {
+        const { products } = get()
+        return products.some((product) => product.id === id)
+      },
+
+      updateCart: (status) =>
+        set(() => ({
+          cart: status
+        }))
+    }),
+    {
+      name: 'productStore'
+    }
+  )
+)
